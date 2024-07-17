46918dc4682188de50947ba4ea8586122d967aa0
Felipe Assunção <l_assuncao@hotmail.com>
2024-07-16 22:30:15 -0300

    mais algumas configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Planejaê - Seu próximo destino começa aqui!",
+  title: "Planejaê: Seu próximo destino começa aqui!",
   description:
     "Planejaê é a ferramenta definitiva para você e seus amigos planejarem o roteiro e organizar os detalhes de suas aventuras.",
 };
